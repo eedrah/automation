7001023969cb064affe2b4029b11b5073525c3a1
Eedrah <9520856+eedrah@users.noreply.github.com>
2020-01-14 22:22:27 +1300

    sort code into tests

--- first.py
+++ first.py
@@ -1,14 +1,33 @@
+import pprint
 import pyautogui
 
-print(pyautogui.position())
-print(pyautogui.size())
-print(pyautogui.PAUSE)
+def testScreenDetails():
+    print(pyautogui.position())
+    print(pyautogui.size())
+    print(pyautogui.PAUSE)
 
-x, y = pyautogui.size()
-pyautogui.moveTo(x / 2, y / 2, duration=3)
+def testMoveMouse():
+    x, y = pyautogui.size()
+    pyautogui.moveTo(x / 2, y / 2, duration=3)
 
-print(pyautogui.screenshot())
+def testScreenshot():
+    print(pyautogui.screenshot())
 
-print(pyautogui.locateOnScreen('file.png'))
-list(pyautogui.locateAllOnScreen('looksLikeThis.png'))
+def testFindImage():
+    print(pyautogui.locateOnScreen('/Users/eedrah/Desktop/point-of-interest.png', grayscale=True, confidence=0.9))
 
+def testFindAllImages():
+    pprint.pprint(list(pyautogui.locateAllOnScreen('/Users/eedrah/Desktop/point-of-interest.png', confidence=0.9)))
+
+def main():
+    # testScreenDetails()
+    # testMoveMouse()
+    # testScreenshot()
+    # testFindAllImages()
+
+    import timeit
+    print(timeit.timeit("testFindImage()", number=10, setup="from __main__ import testFindImage") / 10)
+    # print(sum(timeit.repeat("testFindImage()", number=1, repeat=10, setup="from __main__ import testFindImage")) / 10)
+
+if __name__ == '__main__':
+    main()
